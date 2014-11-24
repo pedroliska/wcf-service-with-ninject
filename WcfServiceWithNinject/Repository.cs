dc920a7c8018f919807500fbc3d2cb4df8cbddfa
Pedro Liska <pedroliska@gmail.com>
2014-11-23 17:17:44 -0700

    things are working

---
+++ WcfServiceWithNinject/Repository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfServiceWithNinject
+{
+public class Repository : IRepository
+{
+    public IEnumerable<Widget> GetWidgets()
+    {
+        return new [] {
+            new Widget {Name = "Widget1", Price=11},
+            new Widget {Name = "Widget2", Price=22}
+        };
+    }
+}
+public interface IRepository
+{
+    IEnumerable<Widget> GetWidgets();
+}
+public class Widget
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
+
+}
---
+++ WcfServiceWithNinject/Service1.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceWithNinject
+{
+public class Service1 : IService1
+{
+    private IRepository _repo;
+    public Service1(IRepository repo)
+    {
+        _repo = repo;
+    }
+    public string TellMeHowManyWidgets()
+    {
+        return string.Format(
+            "We have {0} widgets.", _repo.GetWidgets().Count());
+    }
+    public string GetData(int value)
+    {
+        return string.Format("You entered: {0}", value);
+    }
+
+    public CompositeType GetDataUsingDataContract(CompositeType composite)
+    {
+        if (composite == null)
+        {
+            throw new ArgumentNullException("composite");
+        }
+        if (composite.BoolValue)
+        {
+            composite.StringValue += "Suffix";
+        }
+        return composite;
+    }
+}
+}
